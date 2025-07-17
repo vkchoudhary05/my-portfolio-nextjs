32548ab4a3fe1c19a0ba8534e4454f65bb08d041
vivek <vkchoudhary050607@gmail.com>
2025-07-17 18:14:12 +0530

    resumeUpdate

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -6,7 +6,7 @@ const LocalConfig = {
     NEXT_PUBLIC_GTAG_ID: process.env.NEXT_PUBLIC_GTAG_ID,
     NEXT_PUBLIC_GITHUB_TOKEN: process.env.NEXT_PUBLIC_GITHUB_TOKEN,
     NEXT_PUBLIC_VERCEL_TOKEN: process.env.NEXT_PUBLIC_VERCEL_TOKEN,
-    NEXT_PUBLIC_RESUME_LINK: process.env.NEXT_PUBLIC_RESUME_LINK || "https://drive.google.com/file/d/100jCTFHM3pfpgamuFcOAGSYpIa68fP-p/view?usp=drive_link",
+    NEXT_PUBLIC_RESUME_LINK: process.env.NEXT_PUBLIC_RESUME_LINK || "https://drive.google.com/file/d/1f1LeBBATAEz9jTfkVwpiz_OVEVR65Lyb/view?usp=sharing",
   },
 };
 
